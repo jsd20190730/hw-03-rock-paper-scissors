9b2f41cfbf6f97fb0dbb633d15ba630c10c21f97
Defy1x <mcdonald.cas@yahoo.com>
2019-08-23 18:34:28 -0400

    ready

--- js/main.js
+++ js/main.js
@@ -1,2 +1,72 @@
-$(function () {
-})
+let humanScore = 0;
+let computerScore = 0;
+
+$('#rock').click(playRock);
+$('#paper').click(playPaper);
+$('#scissors').click(playScissors);
+
+function playRock() {
+  play("rock");
+}
+function playPaper() {
+  play("paper");
+}
+function playScissors() {
+  play("scissors");
+}
+
+function play(humanPlay) {
+
+  computerPlay = getComputerPlay();
+
+  $('#status').html("<p>You played <strong>" + humanPlay + "</strong>. The bot played <strong>" + computerPlay + "</strong>.</p>");
+
+  if(humanPlay == 'rock') {
+    if(computerPlay == 'rock') {
+      document.getElementById('status').innerHTML += "<p>You tied. :|</p>";
+    } else if (computerPlay == 'paper') {
+      document.getElementById('status').innerHTML += "<p>You lose. :(</p>";
+      computerScore+=1;
+    } else if (computerPlay == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>You win! :)</p>";
+      humanScore+=1;
+    }
+  } else if (humanPlay == 'paper') {
+    if(computerPlay == 'rock') {
+      document.getElementById('status').innerHTML += "<p>You win! :)</p>";
+      humanScore+=1;
+    } else if (computerPlay == 'paper') {
+      document.getElementById('status').innerHTML += "<p>You tied. :|</p>";
+    } else if (computerPlay == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>You lose. :(</p>";
+      computerScore+=1;
+    }
+  } else if (humanPlay == 'scissors') {
+    if(computerPlay == 'rock') {
+      document.getElementById('status').innerHTML += "<p>You lose. :(</p>";
+      computerScore+=1;
+    } else if (computerPlay == 'paper') {
+      document.getElementById('status').innerHTML += "<p>You win! :)</p>";
+      humanScore+=1;
+    } else if (computerPlay == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>You tied. :|</p>";
+    }
+  }
+
+  $('#humanScore').html(humanScore);
+  $('#computerScore').html(computerScore);
+
+}
+
+function getComputerPlay() {
+  const computerChoice = Math.floor(Math.random() * 3)
+  if (computerChoice === 0) {
+    return 'rock'
+  }
+  if (computerChoice === 1) {
+    return 'paper'
+  }
+  if (computerChoice === 2) {
+    return 'scissors'
+  }
+}
