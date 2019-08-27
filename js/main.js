393b21db72a43475d6a04a3b3623c696969e3d98
Kyle Ligman <kjligman@gmail.com>
2019-08-27 17:36:45 -0400

    evaluation logic and on click humanChoice variables

--- js/main.js
+++ js/main.js
@@ -1,2 +1,37 @@
 $(function () {
+  function randomNumberBetweenZeroThree() {
+     return Math.floor(Math.random() * 3)
+  }
+  // let computerChoice
+
+
+  $('#rock').click(() => {
+    let humanChoice = 'rock'
+    console.log(humanChoice)
+    console.log(randomNumberBetweenZeroThree())
+  })
+  $('#paper').click(() => {
+    let humanChoice = 'paper'
+    console.log(humanChoice)
+  })
+  $('#scissors').click(() => {
+    let humanChoice = 'scissors'
+    console.log(humanChoice)
+  })
+
+  if (humanChoice === computerChoice) {
+    console.log('tie')
+  } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+    console.log('playerB wins')
+  } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    console.log('playerA wins')
+  } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+    console.log('playerB wins')
+  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    console.log('playerA wins')
+  } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    console.log('playerA wins')
+  } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+    console.log('playerB wins')
+  }
 })
