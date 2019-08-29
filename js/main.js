b653cc5ccc6955ff0345728aa24bcbdaf185a5c3
Kyle Ligman <kjligman@gmail.com>
2019-08-29 16:50:38 -0400

    functionality of evaluation, still need to add dom manipulation

--- js/main.js
+++ js/main.js
@@ -1,37 +1,80 @@
 $(function () {
-  function randomNumberBetweenZeroThree() {
-     return Math.floor(Math.random() * 3)
+  //generate random number
+  function randomNumber() {
+    return Math.floor(Math.random() * 3)
   }
-  // let computerChoice
 
+    //computer choice
+  function computerChooses() {
+    let rando = randomNumber()
+    if (rando === 0) {
+      return 'rock'
+    } else if (rando === 1) {
+      return 'paper'
+    } else {
+      return 'scissors'
+    }
+  }
 
+    //evaluate winner for rock choice
   $('#rock').click(() => {
     let humanChoice = 'rock'
-    console.log(humanChoice)
-    console.log(randomNumberBetweenZeroThree())
+    let computerChoice = computerChooses()
+    if (humanChoice === computerChoice) {
+      console.log('tie')
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+      console.log('playerB wins')
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+      console.log('playerA wins')
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+      console.log('playerB wins')
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+      console.log('playerA wins')
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+      console.log('playerA wins')
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+      console.log('playerB wins')
+    }
   })
+      //evaluate winner for paper choice
   $('#paper').click(() => {
     let humanChoice = 'paper'
-    console.log(humanChoice)
+    let computerChoice = computerChooses()
+    if (humanChoice === computerChoice) {
+      console.log('tie')
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+      console.log('playerB wins')
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+      console.log('playerA wins')
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+      console.log('playerB wins')
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+      console.log('playerA wins')
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+      console.log('playerA wins')
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+      console.log('playerB wins')
+    }
   })
+      //evaluate winner for scissors choice
   $('#scissors').click(() => {
     let humanChoice = 'scissors'
-    console.log(humanChoice)
+    let computerChoice = computerChooses()
+    if (humanChoice === computerChoice) {
+      console.log('tie')
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+      console.log('playerB wins')
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+      console.log('playerA wins')
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+      console.log('playerB wins')
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+      console.log('playerA wins')
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+      console.log('playerA wins')
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+      console.log('playerB wins')
+    }
   })
-
-  if (humanChoice === computerChoice) {
-    console.log('tie')
-  } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-    console.log('playerB wins')
-  } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-    console.log('playerA wins')
-  } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-    console.log('playerB wins')
-  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-    console.log('playerA wins')
-  } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-    console.log('playerA wins')
-  } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-    console.log('playerB wins')
-  }
+  
 })
